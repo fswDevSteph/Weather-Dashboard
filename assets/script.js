1ba69f3ec2f7793e995af681726f7b11228c5f60
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2023-12-16 17:25:51 -0400

    added css variables and font

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
-const message = 'Hello, world!';
+var weatherApiKey = '1e57f9cafb1f7d251058b6d4dccdc7c2';
 
-console.log(message);
+var userInputCity;
+var userInputCountry;
+// sample API call call using just city name
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
